ab8c7e0333b00c6ae8648e87f3052fc14321c763
DeiverV <deiberveranodev@gmail.com>
2021-11-03 18:08:59 -0500

    Nuevo boton para cerrar el menu de reÂporte

--- views/js/report.js
+++ views/js/report.js
@@ -1,19 +1,16 @@
 let button = document.getElementById("boton");
 let report = document.getElementById("reportSection");
-let reporting = false;
+let closeReport = document.getElementById("closereport");
 button.addEventListener('click',(e)=>{
-    if(reporting){
-        report.style.opacity = "0";
+    report.style.display = "flex";
+    setTimeout(()=>{
+        report.style.opacity ="100%";
+    },300);
+    reporting=true;
+})
+closeReport.addEventListener('click',(e)=>{
+    report.style.opacity = "0";
         setTimeout(function(){
             report.style.display ="none";
         },300);
-        reporting=false;
-    }else{
-        report.style.display = "flex";
-        setTimeout(()=>{
-            report.style.opacity ="100%";
-        },300);
-        reporting=true;
-    }
 })
-
