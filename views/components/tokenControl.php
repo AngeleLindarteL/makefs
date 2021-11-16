f973c278faddbcd77aad9b21234ea026367dbf97
AngeleLindarteL <angelgamerlolwtf@gmail.com>
2021-11-16 11:29:01 -0500

    Token control added tokenGen

--- views/components/tokenControl.php
+++ views/components/tokenControl.php
@@ -1,10 +1,17 @@
 <?php
-    if (session_status() == PHP_SESSION_NONE) {
+    if (session_status() === PHP_SESSION_NONE) {
         echo <<<EOT
             <script>
                 if(window.localStorage.getItem("token")){
                     window.localStorage.removeItem("token");
                 }
+            </script>
+        EOT;
+    }else{
+        session_start();
+        echo <<<EOT
+            <script>
+                window.localStorage.setItem("token", $_SESSION[token]);
             </script>
         EOT;
     }
