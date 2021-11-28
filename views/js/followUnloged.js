0b84c14caaa874b02b6854d0e3870602d62941e5
DeiverV <deiberveranodev@gmail.com>
2021-11-28 11:26:23 -0500

    Dinamic data in chef-view, valoration and num of recipes

---
+++ views/js/followUnloged.js
@@ -0,0 +1,25 @@
+let btnFollowUnloged = document.querySelector("#followUnloged");
+let sectionFollowUnloged = document.querySelector("#unlogedFollowSect");
+let btnCloseFollowUnloged = document.querySelector("#close-UnlogedFollow");
+let buttonGoToLog = document.querySelector("#logRedirect")
+
+buttonGoToLog.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.href="../views/login.php";
+})
+
+btnFollowUnloged.addEventListener("click",(e)=>{
+    e.preventDefault();
+    sectionFollowUnloged.style.display="flex";
+    setTimeout(()=>{
+        sectionFollowUnloged.style.opacity="100%"
+    },300)
+})
+
+btnCloseFollowUnloged.addEventListener("click",(e)=>{
+    e.preventDefault();
+    sectionFollowUnloged.style.opacity="0";
+    setTimeout(()=>{
+        sectionFollowUnloged.style.display="none";
+    },300)
+})
