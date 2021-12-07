f0b5d8867b2a45943fd88303f579735f73e8bd12
DeiverV <deiberveranodev@gmail.com>
2021-12-07 15:30:56 -0500

    fixed menu desplegable

--- views/newRecipe.php
+++ views/newRecipe.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Recipe!</title>
+    <link rel="stylesheet" href="./css/chef-index.css">
     <link rel="stylesheet" href="./css/normalize.css">
     <link rel="stylesheet" href="./css/newRecipeCss.css">
     <link rel="stylesheet" href="./css/footer.css">
