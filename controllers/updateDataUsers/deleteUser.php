fb4a129668d3cf5027f0786000249de1cea0e863
AngeleLindarteL <angelgamerlolwtf@gmail.com>
2021-12-11 17:41:41 -0500

    Cuando el usuario elimina su cuenta, se borra también sus datos del algoritmo y también se borran sus fotos

--- controllers/updateDataUsers/deleteUser.php
+++ controllers/updateDataUsers/deleteUser.php
@@ -5,7 +5,7 @@ include("../../models/conexion.php");
 
         $connObj = new Conexion;
         $conn = $connObj -> Conectar();
-        $consulta = "SELECT passwordm FROM userm WHERE userid = :id ";
+        $consulta = "SELECT passwordm,minpic,midpic FROM userm WHERE userid = :id ";
         
         try{
             $password = $conn->prepare($consulta);
@@ -21,6 +21,21 @@ include("../../models/conexion.php");
         if(password_verify($_POST["deleteUserPass"],$passwordm["passwordm"])){
             $consultaDelete = "DELETE FROM userm WHERE userid = :id";
             try{
+                $url = "https://makefsapi.herokuapp.com/user/$_SESSION[id]";
+
+                $ch = curl_init($url);
+
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+                curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+                $response = curl_exec($ch);
+                curl_close($ch);
+                if($response == false || $response == "False" || $response == "false"){
+                    return;
+                    exit;
+                }
+                unlink( "../mediaDB/usersImg/" . $passwordm["minpic"]);
+                unlink( "../mediaDB/usersImg/" . $passwordm["midpic"]);
                 $delete = $conn->prepare($consultaDelete);
                 $conn->beginTransaction();
                 $delete->execute(array(":id"=>$_SESSION["id"]));
