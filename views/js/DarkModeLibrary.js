ff7c718a21ffe5b09f2199af24fafe2c90dc5cd4
AngeleLindarteL <angelgamerlolwtf@gmail.com>
2021-12-11 22:32:15 -0500

    bug fix xD

--- views/js/DarkModeLibrary.js
+++ views/js/DarkModeLibrary.js
@@ -7,18 +7,6 @@ let recipetitle = document.querySelector('.WhiteRecipelib');
 let vidsav = document.querySelector('.Whitevidsav');
 let star = document.querySelectorAll('.star-template');
 let trecipe = document.querySelectorAll('.next-text-recipe');
-const url = window.location.pathname.split("/");
-url.shift();
-let viewsUrl;
-
-if(url.length > 1 && url.length < 3){
-    viewsUrl = "../views";
-    console.log(window.location.pathname.split("/").length)
-}else if(url.length > 2){
-    viewsUrl = "../../views";
-}else{
-    viewsUrl = "views";
-}
 /*--Code--*/
 if (localStorage.getItem("Theme") == null){
     localStorage.setItem("Theme", "claro");
